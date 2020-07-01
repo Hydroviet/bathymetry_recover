f15ee66af22b1d8ca8e972f04dad741e17c333f6
htnu <thiennust274@gmail.com>
2020-07-01 13:12:26 +0000

    add shuffle

--- generate_flist.py
+++ generate_flist.py
@@ -12,7 +12,7 @@ parser.add_argument('--src', default='./training_data', type=str,
 parser.add_argument('--dest', default='./data_flist', type=str,
                     help='The folder destination path')
 
-def write_flist(src, dest):
+def write_flist(src, dest, is_val = True):
     files = []
     filename = ''
     print('Reading from ', os.path.join(src))
@@ -20,18 +20,24 @@ def write_flist(src, dest):
     files = list(map(lambda f: os.path.abspath(f), files))
     
     train_fname = os.path.join(dest, 'train.flist')
-    val_fname = os.path.join(dest, 'validation.flist')
+
+    if is_val:
+        val_fname = os.path.join(dest, 'validation.flist')
+        ftrain, fval = train_test_split(files, test_size=0.1, shuffle=True)
     
-    ftrain, fval = train_test_split(files, test_size=0.1)
-    
-    print('Writing to {}'.format(train_fname))
-    fo = open(train_fname, "w")
-    fo.write("\n".join(sorted(ftrain)))
-    fo.close()
-    print('Writing to {}'.format(val_fname))
-    fo = open(val_fname, "w")
-    fo.write("\n".join(sorted(fval)))
-    fo.close()    
+        print('Writing to {}'.format(train_fname))
+        fo = open(train_fname, "w")
+        fo.write("\n".join(ftrain))
+        fo.close()
+        print('Writing to {}'.format(val_fname))
+        fo = open(val_fname, "w")
+        fo.write("\n".join(fval))
+        fo.close()    
+    else:
+        print('Writing to {}'.format(train_fname))
+        fo = open(train_fname, "w")
+        fo.write("\n".join(files))
+        fo.close()
 
 if __name__ == "__main__":
     args = parser.parse_args()
